0f17a97f54497df8b3f5222ab314f7a2e6bc0fcd
Alex Gyryk <oleksandr.gyryk@couchbase.com>
2017-04-13 13:58:01 -0700

    Global offset parameter

--- run_fakeit.py
+++ run_fakeit.py
@@ -8,6 +8,7 @@ _bucket = None
 _password = None
 _items = None
 _threads = None
+_global_offset = 0
 workdir = "../fakeit"
 
 
@@ -15,7 +16,8 @@ p = subprocess.Popen('make build', shell=True, stdout=subprocess.PIPE, stderr=su
                      cwd=workdir)
 retval = p.wait()
 
-def run(batch_size, total_batches, group, s, b, p):
+
+def run(batch_size, total_batches, group, s, b, p, g):
     conn_str = "-s {} -b {}".format(s,b)
     if p is not None:
         conn_str = "{} -p {}".format(conn_str, p)
@@ -23,7 +25,7 @@ def run(batch_size, total_batches, group, s, b, p):
     for i in range(0, total_batches):
         p = subprocess.Popen("./bin/fakeit -d couchbase "
                              "{} --offset {}".format(conn_str,
-                                                     batch_size*group*total_batches + i*batch_size),
+                                                     batch_size*group*total_batches + i*batch_size + g),
                               shell=True,
                               stdout=subprocess.PIPE,
                               stderr=subprocess.STDOUT,
@@ -33,11 +35,11 @@ def run(batch_size, total_batches, group, s, b, p):
         retval = p.wait()
 
 
-def run_fakeit(s,b,p,i,t):
+def run_fakeit(s,b,p,i,t,g):
     batch_size = 1000
     total_batches = i / (batch_size*t)
     for i in range(0, t):
-        new_thread = Thread(target=run, args=(batch_size, total_batches, i,s,b,p))
+        new_thread = Thread(target=run, args=(batch_size, total_batches, i,s,b,p,g))
         new_thread.start()
 
 
@@ -52,10 +54,12 @@ for i,item in enumerate(sys.argv):
         _items = int(sys.argv[i+1])
     elif item == "-t":
         _threads = int(sys.argv[i+1])
+    elif item == "-g":
+        _global_offset = int(sys.argv[i+1])
 
 
 if _server and _bucket and _items and _threads:
-    run_fakeit(s = _server, b = _bucket, p = _password, i = _items, t = _threads)
+    run_fakeit(s = _server, b = _bucket, p = _password, i = _items, t = _threads, g =_global_offset)
 else:
     print("Usage run_fakeit.py -s <server> -b <bucket_name> -p <bucket_password if any> "
           "-i <items>  -t <thereads>")
