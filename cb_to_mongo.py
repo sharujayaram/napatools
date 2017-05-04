e249dcba32862a62afce58a3a2ca44edb632fdc8
Alex Gyryk <oleksandr.gyryk@couchbase.com>
2017-05-04 13:33:18 -0700

    Added offset to tranfser script

--- cb_to_mongo.py
+++ cb_to_mongo.py
@@ -9,21 +9,21 @@ import datetime
 SERVER = "172.23.100.190"
 
 
-def transfer(threads, customers, cbpassword, cbbucket, mongodb, mongotable):
+def transfer(threads, customers, cbpassword, cbbucket, mongodb, mongotable, offset):
     batch_size = int(customers) / int(threads)
 
     for i in range(0, int(threads)):
         print "process {} started".format(i)
-        new_thread = Thread(target=run_batch, args=(batch_size, i, cbpassword, cbbucket, mongodb, mongotable))
+        new_thread = Thread(target=run_batch, args=(batch_size, i, cbpassword, cbbucket, mongodb, mongotable, offset))
         new_thread.start()
 
 
-def insert_to_mongo(id, doc, mdb, mcoll):
+def insert_to_mongo(id, doc, mcoll):
     post_id = mcoll.insert_one(doc).inserted_id
     print "document {} has been inserted to mongo".format(post_id)
 
 
-def run_batch(size, id, cbpassword, cbbucket, mongodb, mongotable):
+def run_batch(size, id, cbpassword, cbbucket, mongodb, mongotable, offset):
     if cbpassword != "":
         cb = Bucket("couchbase://{}/{}?operation_timeout=10".format(SERVER, cbbucket), password=cbpassword)
     else:
@@ -33,7 +33,7 @@ def run_batch(size, id, cbpassword, cbbucket, mongodb, mongotable):
     mdb = client[mongodb]
     mcollection = mdb[mongotable]
 
-    min = id * size + 1
+    min = id * size + 1 + int(offset)
     max = min + size
 
     for i in range (min, max):
@@ -45,11 +45,11 @@ def run_batch(size, id, cbpassword, cbbucket, mongodb, mongotable):
         y,m,d = doc["dob"].split("-")
         doc["dob"] = datetime.datetime(int(y), int(m), int(d))
 
-        insert_to_mongo(id, doc, mdb, mcollection)
+        insert_to_mongo(id, doc, mcollection)
 
         for order in orders:
             orderdoc = cb.get(order).value
-            insert_to_mongo(id, orderdoc, mdb, mcollection)
+            insert_to_mongo(id, orderdoc, mcollection)
 
 threads = 20
 customers = 10000000
@@ -57,6 +57,7 @@ cbpassword = ""
 cbbucket = "bucket-1"
 mongodb = "soe"
 mongotable = "bucket"
+offset = 0
 
 for i, item in enumerate(sys.argv):
     if item == "-threads":
@@ -71,5 +72,8 @@ for i, item in enumerate(sys.argv):
         mongodb = sys.argv[i + 1]
     elif item == "-mongotable":
         mongotable = sys.argv[i + 1]
+    elif item == "-offset":
+        offset = sys.argv[i + 1]
 
-transfer(threads, customers, cbpassword, cbbucket, mongodb, mongotable)
+
+transfer(threads, customers, cbpassword, cbbucket, mongodb, mongotable, offset)
