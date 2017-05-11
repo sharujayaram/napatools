8f9e7447c1f2d2c40ff2842b226a21686691e8e1
Alex Gyryk <oleksandr.gyryk@couchbase.com>
2017-05-11 14:59:11 -0700

    mongo setup scripts fixes

--- mongo/setup/setup.sh
+++ mongo/setup/setup.sh
@@ -1,12 +1,16 @@
-sudo service mongod stop
-sudo yum erase $(rpm -qa | grep mongodb-org)
-sudo rm -r /var/log/mongodb
-sudo rm -r /var/lib/mongo
+service mongod stop
+yum erase $(rpm -qa | grep mongodb-org)
+rm -r /var/log/mongodb
+rm -r /var/lib/mongo
+rm -r /data/configdb
+rm -r /data/mongodb
 cp mongodb-org-2.6.repo /etc/yum.repos.d/mongodb-org-2.6.repo
 yum install -y mongodb-org-3.2.13 mongodb-org-server-3.2.13 mongodb-org-shell-3.2.13 mongodb-org-mongos-3.2.13 mongodb-org-tools-3.2.13
-mkdir /data/configdb
-mkdir /data/mongodb
-mkdir /data/mongodb/shard190
-mkdir /data/mongodb/shard191
-mkdir /data/mongodb/shard192
-mkdir /data/mongodb/shard193
+cd /data
+mkdir configdb
+mkdir mongodb
+cd /data/mongodb
+mkdir shard190
+mkdir shard191
+mkdir shard192
+mkdir shard193
