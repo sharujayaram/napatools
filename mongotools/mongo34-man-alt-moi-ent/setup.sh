6ededc156979df4502295df45ad7476022c8d81a
Alex Gyryk <oleksandr.gyryk@couchbase.com>
2017-06-04 12:09:07 -0700

    inMem mongo setup

---
+++ mongotools/mongo34-man-alt-moi-ent/setup.sh
@@ -0,0 +1,23 @@
+set -x
+
+service mongod stop
+killall -9 mongod
+yum erase $(rpm -qa | grep mongodb-org)
+rm -rf /var/log/mongo
+rm -rf /var/log/mongod
+rm -rf /var/lib/mongo
+rm -rf /var/lib/mongod
+rm -rf /data/configdb
+rm -rf /data/mongodb
+cp mongodb-enterprise.repo /etc/yum.repos.d/mongodb-enterprise.repo
+yum install -y mongodb-enterprise
+cd /data
+mkdir configdb
+mkdir mongodb
+cd /data/mongodb
+mkdir shard204
+mkdir shard205
+mkdir shard206
+mkdir shard207
+
+ulimit -n 32000
