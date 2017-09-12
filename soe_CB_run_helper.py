c8ddb11b18ef40164c0cc93c19c0c6304311e533
Alex Gyryk <oleksandr.gyryk@couchbase.com>
2017-09-11 21:42:04 -0700

    bugfix-sync

--- soe_CB_run_helper.py
+++ soe_CB_run_helper.py
@@ -111,7 +111,7 @@ def action_run():
                 t = int(threads/4)
             else:
                 t = threads - int(threads/4) *3
-            sub_offset = insertstart + ((int(totalrecords) / 10) * (i-1))
+            sub_offset = int(insertstart) + ((int(totalrecords) / 10) * (i-1))
             new_thread = Thread(target=run_thread, args=(workload, host, t, kv, log, i, sub_offset,
                                                          maxexecutiontime, totalrecords))
             new_thread.start()
