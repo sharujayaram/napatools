f5402e71a0ec8d88f6004f8040c5e44bd6c25079
Alex Gyryk <oleksandr.gyryk@couchbase.com>
2017-09-14 10:34:17 -0700

    Update Mongo tools for soe10k 7

--- mongotools/mongo34-man-alt-moi-ent/setup-ubuntu.sh
+++ mongotools/mongo34-man-alt-moi-ent/setup-ubuntu.sh
@@ -10,11 +10,11 @@ rm -rf /data/mongodb
 service mongod stop
 killall -9 mongod
 
-sudo apt-get remove -y mongodb-enterprise
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6
+apt-get remove mongodb-enterprise
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6
 echo "deb [ arch=amd64,arm64,ppc64el,s390x ] http://repo.mongodb.com/apt/ubuntu xenial/mongodb-enterprise/3.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-enterprise.list
-sudo apt-get update
-sudo apt-get install -y mongodb-enterprise
+apt-get update
+apt-get install -y mongodb-enterprise
 
 
 cd /data
