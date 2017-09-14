424274f8fac35040e5e2938ee4bda8f8a4c6f48f
Alex Gyryk <oleksandr.gyryk@couchbase.com>
2017-09-14 13:19:32 -0700

    Update Mongo tools for soe10k 10

--- soe_Mongo_run_helper.py
+++ soe_Mongo_run_helper.py
@@ -6,34 +6,9 @@ import time
 
 MONGODBURL= "mongodb://localhost:27021/ycsb?w=1"
 
-def action_load():
-    total = ""
-    cached = ""
-    host = ""
-    workload = ""
-    insertstart = ""
-    for i, item in enumerate(sys.argv):
-        if item == "-total_items":
-            total = sys.argv[i+1]
-        elif item == "-cache_items":
-            cached = sys.argv[i+1]
-        elif item == "-workload":
-            workload = sys.argv[i+1]
-        elif item == "-insertstart":
-            insertstart = sys.argv[i+1]
 
-    cmd = "./bin/ycsb load mongodb -P workloads/soe/{} -p mongodb.url={} -p operationcount=1 -p recordcount={} " \
-          "-p totalrecordcount={} -threads 20 -p insertstart={}".format(workload, MONGODBURL, cached,
-                                                                        total, insertstart)
-    print "Executing command: {}".format(cmd)
-    p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, cwd="../YCSB")
-    for line in p.stdout.readlines():
-        print line,
-    retval = p.wait()
-
-
-def run_thread(workload, t, log, i, insertstart):
-    cmd = get_ycsb_run_cmd(workload, t, "{}_i{}".format(log, i), insertstart)
+def run_thread(workload, t, log, i, insertstart,  maxexecutiontime, totalrecords):
+    cmd = get_ycsb_run_cmd(workload, t, "{}_i{}".format(log, i), insertstart,  maxexecutiontime, totalrecords)
     print "Executing command: {}".format(cmd)
     p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT,
                          cwd="../clones/YCSB_{}".format(i))
@@ -44,9 +19,9 @@ def run_thread(workload, t, log, i, insertstart):
 
 def action_run():
     workload = ""
-    host = ""
+    maxexecutiontime = ""
     threads = 0
-    kv = ""
+    totalrecords = ""
     log = ""
     insertstart = ""
     for i, item in enumerate(sys.argv):
@@ -58,6 +33,11 @@ def action_run():
             log = sys.argv[i+1]
         elif item == "-insertstart":
             insertstart = sys.argv[i+1]
+        elif item == "-test_duration":
+            maxexecutiontime = sys.argv[i + 1]
+        elif item == "-total_items":
+            totalrecords = sys.argv[i + 1]
+
 
     if threads > 35:
         for i in (1,2,3,4):
@@ -66,27 +46,26 @@ def action_run():
                 t = int(threads/4)
             else:
                 t = threads - int(threads/4) *3
-            new_thread = Thread(target=run_thread, args=(workload, t, log, i, insertstart))
+            new_thread = Thread(target=run_thread, args=(workload, t, log, i, insertstart,  maxexecutiontime,
+                                                         totalrecords))
             new_thread.start()
     else:
-        cmd = get_ycsb_run_cmd(workload, threads, log, insertstart)
+        cmd = get_ycsb_run_cmd(workload, threads, log, insertstart, maxexecutiontime, totalrecords)
         print "Executing command: {}".format(cmd)
         p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, cwd="../YCSB")
         for line in p.stdout.readlines():
             print line,
         retval = p.wait()
 
+def get_ycsb_run_cmd(workload, threads, log, insertstart, maxexecutiontime, totalrecords):
+    return "./bin/ycsb run mongodb -P workloads/soe/{} -p mongodb.url={} " \
+           "-p operationcount=900000000 -p maxexecutiontime={} -threads {} " \
+           "-p exportfile=../{}.log -p insertstart={} -p recordcount={}".format(workload,
+                                                                                MONGODBURL,
+                                                                                maxexecutiontime,
+                                                                                threads,
+                                                                                log,
+                                                                                insertstart,
+                                                                                totalrecords)
 
-def get_ycsb_run_cmd(workload, threads, log, insertstart):
-    return "./bin/ycsb run mongodb -P workloads/soe/{} -p mongodb.url={} -p operationcount=900000000 " \
-           "-p maxexecutiontime=600 -threads {} -p exportfile=../{}.log -p insertstart={}".format(workload,
-                                                                                                  MONGODBURL,
-                                                                                                  threads,
-                                                                                                  log,
-                                                                                                  insertstart)
-for i,item in enumerate(sys.argv):
-    if item == "-action":
-        if sys.argv[i+1] == "load":
-            action_load()
-        elif sys.argv[i+1] == "run":
-            action_run()
+action_run()
