84ee8d2fb48692d5d886c026a2963d3ab75aa308
Alex Gyryk <oleksandr.gyryk@couchbase.com>
2017-09-19 10:26:55 -0700

    sync

--- makeclones.sh
+++ makeclones.sh
@@ -1,13 +1,13 @@
 cd ..
-rm -rf ../YCSB_*
-cp -r ../YCSB ../YCSB_0
-cp -r ../YCSB ../YCSB_1
-cp -r ../YCSB ../YCSB_2
-cp -r ../YCSB ../YCSB_3
-cp -r ../YCSB ../YCSB_4
-cp -r ../YCSB ../YCSB_5
-cp -r ../YCSB ../YCSB_6
-cp -r ../YCSB ../YCSB_7
-cp -r ../YCSB ../YCSB_8
-cp -r ../YCSB ../YCSB_9
-cp -r ../YCSB ../YCSB_10
+rm -rf YCSB_*
+cp -r YCSB YCSB_0
+cp -r YCSB YCSB_1
+cp -r YCSB YCSB_2
+cp -r YCSB YCSB_3
+cp -r YCSB YCSB_4
+cp -r YCSB YCSB_5
+cp -r YCSB YCSB_6
+cp -r YCSB YCSB_7
+cp -r YCSB YCSB_8
+cp -r YCSB YCSB_9
+cp -r YCSB YCSB_10
