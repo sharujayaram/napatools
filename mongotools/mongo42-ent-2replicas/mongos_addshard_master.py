831ff086a434ba82b3e0ab39be36616cbcbf2ec7
sharath chandra <sharujayaram@gmail.com>
2019-08-29 12:29:58 -0700

    mongos add shard step automated

---
+++ mongotools/mongo42-ent-2replicas/mongos_addshard_master.py
@@ -0,0 +1,16 @@
+from pymongo import MongoClient
+
+c = MongoClient('localhost', 27021)
+
+
+c.admin.command('addshard', 'SHRD204/172.23.100.204:27014')
+
+c.admin.command('addshard', 'SHRD205/172.23.100.205:27015')
+
+c.admin.command('addshard', 'SHRD206/172.23.100.206:27016')
+
+c.admin.command('addshard','SHRD207/172.23.100.207:27017')
+
+
+c.admin.command('enablesharding', 'ycsb')
+
