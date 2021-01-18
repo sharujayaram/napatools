afa2239cc0a7c1b25a472e185135fa983ecc1107
sharath chandra <sharujayaram@gmail.com>
2021-01-18 12:20:11 -0800

    adding replicat_set config for single primary and single secondary

--- mongotools/mongo42-ent-2replicas/init_shard_replicaset_204.py
+++ mongotools/mongo42-ent-2replicas/init_shard_replicaset_204.py
@@ -4,7 +4,6 @@ c = MongoClient('172.23.100.121', 27014)
 
 config = { '_id': "SHRD204",
            'members': [ { '_id' : 0, 'host' : "172.23.100.121:27014" },
-                      { '_id' : 1, 'host' : "172.23.100.121:27015" },
-                      { '_id' : 2, 'host' : "172.23.100.121:27016" } ] }
+                      { '_id' : 1, 'host' : "172.23.100.121:27015" } ] }
 
 c.admin.command("replSetInitiate", config)
